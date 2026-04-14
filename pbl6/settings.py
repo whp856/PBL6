089fe69127fb2b1afc4e8f67bdd178837b50b93b
whp <3622075226@qq.com>
2026-04-14 19:38:06 +0800

    添加template

--- pbl6/settings.py
+++ pbl6/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'pbl6.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'pbl6' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
